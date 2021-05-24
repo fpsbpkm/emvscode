6d61aaf483d8dccc64b7303fed999b5b62459285
fpsbpkm <taisei4510471a@yahoo.co.jp>
2021-05-24 12:01:42 +0900

    コメントの追加

--- lint/test.py
+++ lint/test.py
@@ -6,18 +6,16 @@ _ERROR_CATEGORIES = [
 
 _line_length = 80
 
-# cpplintの例
+# cpplintの出力例(eclipse)
 # test.c:4: warning: Lines should be <= 80 characters long  [whitespace/line_length] [2]
 def Error(filename, linenum, category, confidence, message):
     sys.stderr.write(f"{filename}:{linenum}: warning: {message} [{category}] [{confidence}]\n")
-
 
 def CheckStyle(filename, linenum, contents):
     linewidth = len(contents)
     if linewidth > _line_length:
         Error(filename, linenum, 'whitespace/line_length', 2,
             'Lines should be <= %i characters long' % _line_length)
-
 
 def ProcessFileData(filename):
     linenum = 0
@@ -28,12 +26,10 @@ def ProcessFileData(filename):
                 break
             linenum += 1
             CheckStyle(filename, linenum, contents)    
-    
 
 def main():
     filename = sys.argv[1]
     ProcessFileData(filename)
-
 
 if __name__ == '__main__':
     main()
